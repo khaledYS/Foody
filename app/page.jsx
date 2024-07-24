a14e46ab0d320a723d371a4bf9937ebd9f5aeb2d
ammar <ammar@gmail.com>
2024-07-24 20:04:50 +0300

    made the landing page more responsive, flipped the "What is foody" section in small screens to make it responsive, changed the "customer feedback" icon.

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,13 @@ import Feature from "./Feature";
 import { HiOutlineQrcode } from "react-icons/hi";
 import { TfiTablet } from "react-icons/tfi";
 import { FaComments, FaUserClock } from "react-icons/fa";
+import { PiStarFill } from "react-icons/pi";
 
 const page = () => {
   return(
-    <div className="">
+    <div className="body">
 
-      <div className="w-full h-[85vh] grid place-items-center">
+      <div className="w-full h-[85vh] grid place-items-center" id="home" >
         <div className="my-10 flex flex-col justify-center items-center max-w-[40vw] max-md:max-w-full max-md:px-6 mx-auto text-center text-[18px] gap-2">
           <h1 className="text-[48px] font-semibold ">Create Your <span className="text-prime">Menu</span> Faster!</h1>
           <h3 className="">
@@ -18,12 +19,12 @@ const page = () => {
         </div>
       </div>
 
-      <div className="flex w-full px-16 max-md:px-8 max-md:my-12">
-        <div className="w-full flex py-8 max-md:flex-col">
-          <div className="w-full">
+      <div className="flex h-[90vh] w-full px-16 max-md:px-8 max-md:my-12" id="aboutus">
+        <div className="w-full flex py-8 max-md:flex-col-reverse">
+          <div className="w-full flex items-center">
             <div className="w-full mr-2 max-w-xl">
               <h1 className="text-[36px] my-4">What is Foody?</h1>
-              <p className="text-[18px] my-10 ml-6">
+              <p className="text-[18px] my-10 ml-6 max-md:ml-3 max-md:mt-5">
                 Foody simplifies restaurant and cafe management with tools for digital menus, online orders, reservations, and staff scheduling. Customers can access menus and order via QR codes. Our feedback system offers insights to improve service, while employee scheduling and table management ensure smooth operations. Enhance efficiency and customer experience with Foody.
               </p>
             </div>
@@ -33,10 +34,10 @@ const page = () => {
         </div>
       </div>
 
-      <dir className="h-screen w-screen grid place-items-center text-secnd p-0 max-md:my-12">
+      <dir className="h-screen w-screen grid place-items-center text-secnd p-0 max-md:my-12" id="features">
         <div className="w-full flex flex-col text-center justify-center items-center gap-12 max-md:gap-6">
           <div className="text-h2 max-md:text-h3">What does it provide? </div>
-          <div className="flex gap-8 max-md:gap-4 flex-wrap justify-center px-4">
+          <div className="flex gap-8 max-md:gap-4 flex-wrap justify-center px-4 pb-10">
             <Feature 
               title="Menu Maker" 
               description="Create and manage digital menus with QR code integration for easy customer access." 
@@ -52,12 +53,12 @@ const page = () => {
             <Feature 
               title="Customer Feedback" 
               description="Collect and analyze customer reviews and ratings to improve your service." 
-              logo={<FaComments />} />
+              logo={<PiStarFill  />} />
           </div>
         </div>
       </dir>
 
     </div>
   );
-}
+};
 export default page;
